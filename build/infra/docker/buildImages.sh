6468f8a8971aaca27433e5eaa704b33b8f440a7b
vm <vm@qpointz.io>
2020-08-20 22:22:12 +0200

    [deploy] docker compose setup for local development

---
+++ build/infra/docker/buildImages.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+rootDir=`dirname $0`
+
+function buildImg {
+    local imgName=${1}
+    local tagName=${2}
+    docker build --tag ${tagName} ${rootDir}/images/${imgName}
+}
+
+function pushImg {
+    local tagName=${1}
+    docker push ${tagName}
+}
+
+function buildAndPushImg {
+    local imgName=${1}
+    local tagName="docker.qpointz.io/${imgName}"
+    buildImg ${imgName} ${tagName}
+    pushImg ${tagName}
+}
+
+
+buildAndPushImg enzyme-ci-npm
+buildAndPushImg enzyme-ci-sbt
+buildAndPushImg hadoop-unit
+buildAndPushImg enzyme-ci-sphinx
+#buildAndPushImg postgres-test
