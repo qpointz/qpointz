83a0086b6a8696622090f2f621a58a9ece719ac8
vm <vm@qpointz.io>
2021-12-21 12:49:53 +0100

    [new] Initial QId implementation

--- flow/flow-core/src/main/scala/io/qpointz/flow/QId.scala
+++ flow/flow-core/src/main/scala/io/qpointz/flow/QId.scala
@@ -16,70 +16,23 @@
 
 package io.qpointz.flow
 
+import java.net.URI
 import scala.util.matching.Regex
 
 trait QId
-case class Id(ns:String, hierarchy:Seq[String], group:String, typeName:String, id:String) extends QId
-case class TypeId(ns:String, hierarchy:Seq[String], group:String, typeName:String) extends QId
-case class GroupId(ns:String, hierarchy:Seq[String], group:String) extends QId
-case class HierarchyId(ns:String, hierarchy:Seq[String]) extends QId
-case class NamespaceId(ns:String) extends QId
+trait QNamespaceId extends QId {val ns:String}
+trait QHierarchyId extends QNamespaceId {val hierarchy:Seq[String]}
+trait QGroupId extends QHierarchyId {val group:String}
+trait QTypeId extends QGroupId {val typeName:String}
+trait QRefId  extends QTypeId {val rid:String}
+
+case class NamespaceId(ns:String) extends QNamespaceId
+case class HierarchyId(ns:String, hierarchy:Seq[String]) extends QHierarchyId
+case class GroupId(ns:String, hierarchy:Seq[String], group:String) extends QGroupId
+case class TypeId(ns:String, hierarchy:Seq[String], group:String, typeName:String) extends QTypeId
+case class RefId(ns:String, hierarchy:Seq[String], group:String, typeName:String, rid:String) extends QRefId
 
 object QId {
-
-  private type ParseTuple = Option[(Option[String], Seq[String], Option[String], Option[String], Option[String])]
-
-  //scalastyle:off
-  private val idr = """^(qp\:){0,1}(?<ns>[a-zA-Z][a-zA-Z0-9]*)(?<h>((?>\/)([a-zA-Z][a-zA-Z0-9]*))*)(?>\/)(?<g>[a-zA-Z][a-zA-Z0-9]*){1}(?>\/)(?<tn>[a-zA-Z][a-zA-Z0-9]*)((?>\#)(?<id>[a-zA-Z][a-zA-Z0-9]*))*$""".r
-  //scalastyle:on
-
-  //^(?>qp\:\/\/)(?<ns>[a-zA-Z][a-zA-Z0-9]*)(?<h>((?>\/)([a-zA-Z][a-zA-Z0-9]*))*)(?>\:)(?<g>[a-zA-Z][a-zA-Z0-9]*)(?>\:)(?<tn>[a-zA-Z][a-zA-Z0-9]*)((?>\#)(?<id>[a-zA-Z][a-zA-Z0-9]*))*$
-
-  //goodone
-  //^(?>qp\:)(?<ns>\/\/[a-zA-Z][a-zA-Z0-9]*){1}(?<h>(\/([a-zA-Z][a-zA-Z0-9]*))*)(?<g>(\:([a-zA-Z][a-zA-Z0-9]*)){0,2})(?<id>\#[a-zA-Z][a-zA-Z0-9]*){0,1}$
-
-  private def parse(in:String): ParseTuple = {
-    def mapToId(m: Regex.Match): ParseTuple = {
-      def asOp(gn: String) = {
-        m.group(gn) match {
-          //scalastyle:off
-          case null => None
-          //scalastyle:on
-          case es if es.isEmpty => None
-          case s => Some(s)
-        }
-      }
-      val hs:Seq[String] = m.group("h") match {
-        case x if x.length > 0 => x.split(raw"\/").filter(_.nonEmpty).toSeq
-        case _ => Seq()
-      }
-      Some((asOp("ns"), hs, asOp("g"), asOp("tn"), asOp("id")))
-    }
-
-    idr.findFirstMatchIn(in) match {
-      case Some(r) => mapToId(r)
-      case None => None
-    }
-  }
-
-  def fromString(in:String):QId = fromStringOp(in) match {
-    case Some(id) => id
-    case _ => throw new IllegalArgumentException(s"${in} can't be parsed. expected format [qp:]<namespace>/<hierarchy>/*<group>/<typename>[#id]")
-  }
-
-  def fromStringOp(in: String): Option[QId] = {
-      parse(in) match {
-        case Some(x) => x match {
-          case (Some(ns), h, Some(g), Some(tn), Some(id))   => Some(Id(ns, h, g, tn, id))
-          case (Some(ns), h, Some(g), Some(tn), None)       => Some(TypeId(ns, h, g, tn))
-          case (Some(ns), h, Some(g), None, None)           => Some(GroupId(ns, h, g))
-          case (Some(ns), h, None, None, None)              => Some(HierarchyId(ns, h))
-          case (Some(ns), h, None, None, None) if h.isEmpty => Some(NamespaceId(ns))
-          case _                                            => None
-        }
-        case None => None
-      }
-  }
 
   implicit class QIdMethods(qid:QId) {
 
@@ -87,12 +40,31 @@ object QId {
       if (pf.isDefinedAt(qid)) pf(qid ) else false
     }
 
-    def isId:Boolean            = is {case _:Id => true}
+    def isRefId:Boolean            = is {case _:RefId => true}
     def isTypeId:Boolean        = is {case _:TypeId => true}
     def isGroupId:Boolean       = is {case _:GroupId => true}
     def isHierarchyId:Boolean   = is {case _:HierarchyId => true}
     def isNamespaceId:Boolean   = is {case _:NamespaceId => true}
 
+    def toURI:URI = {
+      def hToString(h:Seq[String]) = h match {
+        case l if l.isEmpty => ""
+        case l => l.mkString("/","/","")
+      }
+      val s = qid match {
+        case qid:RefId        => s"qp:${qid.ns}${hToString(qid.hierarchy)}:${qid.group}:${qid.typeName}#${qid.rid}"
+        case qid:TypeId       => s"qp:${qid.ns}${hToString(qid.hierarchy)}:${qid.group}:${qid.typeName}"
+        case qid:GroupId      => s"qp:${qid.ns}${hToString(qid.hierarchy)}:${qid.group}"
+        case qid:HierarchyId  => s"qp:${qid.ns}${hToString(qid.hierarchy)}"
+        case qid:NamespaceId  => s"qp:${qid.ns}"
+      }
+      URI.create(s)
+    }
   }
+
+  def fromStringOp(in:String):Option[QId] = QIdParser.parseOp(in)
+
+  def fromString(in:String):QId = QIdParser.parse(in)
+
 }
 
