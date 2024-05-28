630fbcdfd54b44e9cf444eacbe563320e34b4eab
vm <vm@qpointz.io>
2024-05-28 13:56:55 +0200

    [fix:build] change release configuration, clean up files [build:dependencies] bump dependencies

--- delta/delta-service-core/src/main/java/io/qpointz/delta/service/DeltaServiceBase.java
+++ delta/delta-service-core/src/main/java/io/qpointz/delta/service/DeltaServiceBase.java
@@ -14,18 +14,10 @@ import java.util.List;
 import java.util.function.Function;
 
 @Slf4j
-@AllArgsConstructor
-@GrpcService
-public class DeltaServiceBase extends DeltaServiceGrpc.DeltaServiceImplBase {
+public class DeltaServiceBase {
 
-    @Getter(AccessLevel.PROTECTED)
-    private final SchemaProvider schemaProvider;
-
-    @Getter(AccessLevel.PROTECTED)
-    private final ExecutionProvider executionProvider;
-
-    @Getter(AccessLevel.PROTECTED)
-    private final SqlParserProvider sqlParserProvider;
+    private DeltaServiceBase() {
+    }
 
     private boolean supportsSql() {
         return this.sqlParserProvider != null && sqlParserProvider.getAcceptsSql()
@@ -36,237 +28,7 @@ public class DeltaServiceBase extends DeltaServiceGrpc.DeltaServiceImplBase {
         return this.getExecutionProvider() != null && this.executionProvider.canExecuteSubstraitPlan();
     }
 
-    @Override
-    public void handshake(HandshakeRequest request, StreamObserver<HandshakeResponse> responseObserver) {
-        process(request, responseObserver, this::onHandshake);
-    }
-    protected HandshakeResponse onHandshake(HandshakeRequest request) {
-        val capabilities = HandshakeResponse.Capabilities.newBuilder()
-                .setSupportSql(this.supportsSql())
-                .setSupportPlan(this.supportsSubstraitPlan())
-                .setSupportJsonPlan(false)
-                .build();
-
-        val authCtx = SecurityContextHolder.getContext();
-        var principal = "ANONYMOUS";
-        if (authCtx!=null && authCtx.getAuthentication()!=null) {
-           principal = authCtx.getAuthentication().getPrincipal().toString();
-        }
-
-        val auth = HandshakeResponse.SecurityContext.newBuilder()
-                .setPrincipal(principal)
-                .build();
-
-        return HandshakeResponse.newBuilder()
-                .setVersion(ProtocolVersion.V1_0)
-                .setStatus(ResponseStatuses.ok())
-                .setCapabilities(capabilities)
-                .setSecurity(auth)
-                .build();
-    }
-
-    @Override
-    public void listSchemas(ListSchemasRequest request, StreamObserver<ListSchemasResponse> responseObserver) {
-        process(request, responseObserver, this::onListSchemas);
-    }
-
-    protected ListSchemasResponse onListSchemas(ListSchemasRequest listSchemasRequest) {
-        return ListSchemasResponse.newBuilder()
-                .setStatus(ResponseStatuses.ok())
-                .addAllSchemas(this.getSchemaProvider().getSchemaNames())
-                .build();
-    }
-
-    @Override
-    public void getSchema(GetSchemaRequest request, StreamObserver<GetSchemaResponse> responseObserver) {
-        process(request, responseObserver, this::onGetSchema);
-    }
-
-    protected GetSchemaResponse onGetSchema(GetSchemaRequest request) {
-        val builder = GetSchemaResponse.newBuilder();
-        val schema = this.getSchemaProvider().getSchema(request.getSchemaName());
-
-        if (schema.isEmpty()) {
-            val message = String.format("Schema '%s' not found", request.getSchemaName());
-            return builder
-                    .setStatus(ResponseStatuses.invalidRequest(message))
-                    .build();
-        } else {
-            return builder
-                    .setStatus(ResponseStatuses.ok())
-                    .setSchema(schema.get())
-                    .build();
-        }
-    }
-
-    @Override
-    public void prepareStatement(PrepareStatementRequest request, StreamObserver<PrepareStatementResponse> responseObserver) {
-        process(request, responseObserver, this::onPrepareStatement);
-    }
-
-    @AllArgsConstructor
-    @Builder
-    public static class PrepareResult {
-
-        @Getter
-        private Collection<String> errors;
-
-        @Getter
-        private PreparedStatement statement;
-
-        public boolean isSuccess() {
-            return errors==null || errors.size()==0;
-        }
-
-        public static PrepareResult of(Collection<String> errors) {
-            return PrepareResult.builder()
-                    .errors(errors)
-                    .build();
-        }
-
-        public static PrepareResult of(String error) {
-            return PrepareResult.of(List.of(error));
-        }
-
-        public static PrepareResult ok(PreparedStatement preparedStatement) {
-            return PrepareResult.builder()
-                    .errors(List.of())
-                    .statement(preparedStatement)
-                    .build();
-        }
-    }
-
-    protected PrepareStatementResponse onPrepareStatement(PrepareStatementRequest request) {
-        val statement = request.getStatement();
-
-        val builder = PrepareStatementResponse.newBuilder()
-                .setOriginalStatement(statement);
-
-        PrepareResult preparedResult = prepareStatement(statement);
-
-        if (preparedResult.isSuccess()) {
-            return builder
-                    .setStatus(ResponseStatuses.ok())
-                    .setStatement(preparedResult.getStatement())
-                    .build();
-        } else {
-            return builder
-                    .setStatus(ResponseStatuses.error("Validation error", preparedResult.getErrors()))
-                    .build();
-        }
-
-    }
-
-    private PrepareResult prepareStatement(Statement statement) {
-        if (statement.hasSql()) {
-            return prepareAndValidateSql(statement.getSql());
-        }
-
-        if (statement.hasText()) {
-            return prepareAndValidateText(statement.getText());
-        }
-
-        if (statement.hasPlan()) {
-            return prepareAndValidatePlan(statement.getPlan());
-        }
-
-        return PrepareResult.of("Statement expected to provide Sql or Substrait or Text plan.");
-    }
-
-    private PrepareResult prepareAndValidatePlan(PlanStatement statement) {
-        if (!this.supportsSubstraitPlan()) {
-            return PrepareResult.of("Substrait plans not supported");
-        }
-
-        val preparedStatement = PreparedStatement.newBuilder()
-                .setPlan(statement)
-                .build();
-
-        return PrepareResult.ok(preparedStatement);
-    }
-
-    private PrepareResult prepareAndValidateText(TextPlanStatement statement) {
-        return PrepareResult.of("JSON plans not supported");
-    }
-
-    private PrepareResult prepareAndValidateSql(SQLStatement statement) {
-        if (!this.supportsSql()) {
-            return PrepareResult.of("SQL not supported");
-        }
-
-        val parseResult = this.getSqlParserProvider()
-                .parse(statement.getStatement());
-
-        val planStatement = PlanStatement.newBuilder()
-                        .setPlan(parseResult.getPlan())
-                .build();
-
-        val preparedStatement = PreparedStatement.newBuilder()
-                .setPlan(planStatement)
-                .build();
-
-        if (!parseResult.isSuccess()) {
-            return PrepareResult.of(parseResult.getMessage());
-        } else {
-            return PrepareResult.ok(preparedStatement);
-        }
-    }
-
-    @Override
-    public void executeQueryStream(ExecQueryStreamRequest request, StreamObserver<ExecQueryResponse> respObserver) {
-        var observer = (ServerCallStreamObserver<ExecQueryResponse>)respObserver;
-        try {
-            val prepared = request.getStatement();
-            if (prepared.hasPlan()) {
-                if (!this.executionProvider.canExecuteSubstraitPlan()) {
-                    val resp = ExecQueryResponse.newBuilder()
-                                    .setStatus(ResponseStatuses.error("Plan execution not suported"))
-                                    .build();
-                    observer.onNext(resp);
-                    observer.onCompleted();
-                }
-
-                val executor = this.executionProvider
-                        .getSubstraitExecutionProvider();
-                val iterator = executor.execute(prepared.getPlan(), request.getBatchSize());
-
-                streamResult(iterator, observer);
-                return;
-            }
-
-            if (prepared.hasSql()) {
-                if (!this.executionProvider.canExecuteSql()) {
-                    val resp = ExecQueryResponse.newBuilder()
-                            .setStatus(ResponseStatuses.error("Sql execution not supported"))
-                            .build();
-                    observer.onNext(resp);
-                    observer.onCompleted();
-                }
-
-                val executor = this.executionProvider
-                        .getSqlExecutionProvider();
-                val iterator = executor.execute(prepared.getSql(), request.getBatchSize());
-
-                streamResult(iterator, observer);
-                return;
-            }
-
-            val resp = ExecQueryResponse.newBuilder()
-                    .setStatus(ResponseStatuses.error("No Plan nor Sql provided"))
-                    .build();
-            observer.onNext(resp);
-            observer.onCompleted();
-        }
-        catch (Exception ex) {
-            val status = ResponseStatuses.error(ex);
-            observer.onNext(ExecQueryResponse.newBuilder()
-                            .setStatus(status)
-                                    .build());
-            observer.onCompleted();
-        }
-    }
-
-    private void streamResult(Iterator<VectorBlock> iterator, ServerCallStreamObserver<ExecQueryResponse> observer) {
+    public static void streamResult(Iterator<VectorBlock> iterator, ServerCallStreamObserver<ExecQueryResponse> observer) {
         while (iterator.hasNext()) {
             val resp = ExecQueryResponse.newBuilder()
                     .setStatus(ResponseStatuses.ok())
@@ -276,7 +38,7 @@ public class DeltaServiceBase extends DeltaServiceGrpc.DeltaServiceImplBase {
         observer.onCompleted();
     }
 
-    protected <TResp> void process(StreamObserver<TResp> observer, TResp reply) {
+    public static <TResp> void process(StreamObserver<TResp> observer, TResp reply) {
         if (reply!=null) {
             observer.onNext(reply);
             observer.onCompleted();
@@ -285,7 +47,7 @@ public class DeltaServiceBase extends DeltaServiceGrpc.DeltaServiceImplBase {
         }
     }
 
-    protected <TReq, TResp> void process(TReq request, StreamObserver<TResp> observer, Function<TReq, TResp> onHandle) {
+    public static <TReq, TResp> void process(TReq request, StreamObserver<TResp> observer, Function<TReq, TResp> onHandle) {
         try {
             final var reply = onHandle.apply(request);
             process(observer, reply);
