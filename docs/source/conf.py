851c52840e704ad42ca466f6e7b159526475cda2
vm <vm@qpointz.io>
2024-06-20 14:30:53 +0200

    [build] add rc branch , master branch renamed to main

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -245,10 +245,10 @@ sphinx_tabs_disable_css_loading = True
 smv_tag_whitelist = r'^[\w-]+-v(er)*\d+\.\d+\.\d+(-\w[\w-]+)*$'
 
 # Whitelist pattern for branches (set to None to ignore all branches)
-smv_branch_whitelist = r'^(dev|master)$'
+smv_branch_whitelist = r'^(dev|master|rc)$'
 
 # Whitelist pattern for remotes (set to None to use local branches only)
-smv_remote_whitelist = r'(^tags\/[\w-]+-v(er)*\d+\.\d+\.\d+(-\w[\w-]+)*$)|(^.*origin\/(dev|master)$)'
+smv_remote_whitelist = r'(^tags\/[\w-]+-v(er)*\d+\.\d+\.\d+(-\w[\w-]+)*$)|(^.*origin\/(dev|maste|rc)$)'
 
 # Pattern for released versions
 smv_released_pattern = r'^tags\/release-v(er)*\d+\.\d+\.\d+(-\w[\w-]+)*$'
