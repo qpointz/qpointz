e90b6b8f0a02e36dca894d38425da3457ebe9198
vm <vm@qpointz.io>
2024-10-14 21:49:07 +0200

    [deploy] tf keys should be branch specific

--- mill/clients/mill-py/tests/test_client_http.py
+++ mill/clients/mill-py/tests/test_client_http.py
@@ -9,10 +9,8 @@ class MillHttpClientTests(ClientBaseTests.MillClientTest):
         super().__init__(methodName,"select * from `ts`.`TEST`", "ts", " `ID` < 0 ", "http")
 
     def __client__(self):
-        host = os.environ.get("MILL_HOST", "localhost")
-        port = os.environ.get("MILL_PORT", "7071")
-        #return create_client(url=f"http://{host}:{port}/api/")
-        return create_client(url="https://bckfuncchangefuncjdb-app.azurewebsites.net/api/")
+        url = os.environ.get("MILL_AZ_FUNC_API_URL", "http://localhost:7071/api/")
+        return create_client(url = url)
 
 if __name__ == '__main__':
     run_test_profile("http")
