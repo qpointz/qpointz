d0ec087ad61f3239fad86dede7eb5c5755c82a8a
username <email>
2025-11-25 17:28:44 +0100

    [build] build and publish sample docker images

--- apps/mill-service/build.gradle.kts
+++ apps/mill-service/build.gradle.kts
@@ -16,42 +16,88 @@ springBoot {
     }
 }
 
-copyDistro("installDist", "main" )
-copyDistro("installBootDist", "boot")
+fun copySample(outDirProvider: Provider<Directory>) {
+    val outDir = outDirProvider.get()
+    copy {
+        from(layout.projectDirectory.dir("config/default"))
+        into(outDir.dir("config"))
+    }
+
+    copy {
+        from(project.layout.projectDirectory.dir("config/sample"))
+        into(outDir.dir("etc/sample"))
+    }
+
+    copy {
+        from(rootProject.layout.projectDirectory.dir("../test/datasets/airlines/csv"))
+        into(outDir.dir("etc/sample/airlines"))
+    }
+
+    copy {
+        from(rootProject.layout.projectDirectory.dir("../test/datasets/users/sql"))
+        into(outDir.dir("etc/sample/users"))
+    }
+
+    copy {
+        from(rootProject.layout.projectDirectory.file("../test/datasets/moneta/moneta-slim.sql"))
+        into(outDir.file("etc/sample/moneta-slim"))
+        rename { fileName -> "moneta.sql" }
+    }
+
+    copy {
+        from(rootProject.layout.projectDirectory.dir("../test/datasets/moneta/"))
+        into(outDir.dir("etc/sample/moneta"))
+    }
+}
+
+tasks.register("assembleSamples") {
+    group = "distribution"
+    description = "Installs sample data "
+    doLast {
+        val outDir = project.layout.buildDirectory.dir("install/samples").get()
+        val datasetsDir = rootProject.layout.projectDirectory.dir("../test/datasets/")
+
+        //moneta sample
+        copy {
+            from(datasetsDir.file("moneta/moneta-slim.sql"))
+            into(outDir.file("data/moneta"))
+        }
+
+        copy {
+            from(datasetsDir.file("moneta/moneta.sql"))
+            into(outDir.file("data/moneta"))
+        }
+
+        copy {
+            from(datasetsDir.file("moneta/moneta-meta.yaml"))
+            into(outDir.file("etc"))
+        }
+
+        val monetaSample = project.layout.projectDirectory.dir("src/main/docker/samples")
+        copy {
+            from(monetaSample.file("application-moneta.yml"))
+            from(monetaSample.file("application-moneta-slim.yml"))
+            into(outDir.file("config"))
+        }
+
+    }
+}
+
 
 fun copyDistro(tk:String, distributionName: String) {
     tasks.findByName(tk)!!.doLast {
         logger.warn("Copy copy")
         val distBaseName = distributions.getByName(distributionName).distributionBaseName.get()
         val buildDir = project.layout.buildDirectory
-        val outDir = buildDir.dir("install/${distBaseName}").get()
-        copy {
-            from(layout.projectDirectory.dir("config/default"))
-            into(outDir.dir("config"))
-        }
-
-        copy {
-            from(project.layout.projectDirectory.dir("config/sample"))
-            into(outDir.dir("etc/sample"))
-        }
-
-        copy {
-            from(rootProject.layout.projectDirectory.dir("test/datasets/airlines/csv"))
-            into(outDir.dir("etc/sample/airlines"))
-        }
-
-        copy {
-            from(rootProject.layout.projectDirectory.dir("test/datasets/users/sql"))
-            into(outDir.dir("etc/sample/users"))
-        }
-
-        copy {
-            from(rootProject.layout.projectDirectory.file("test/datasets/moneta/moneta-slim.sql"))
-            into(outDir.file("etc/sample/moneta"))
-            rename { fileName -> "moneta.sql" }
-        }
+        val outDir = buildDir.dir("install/${distBaseName}")
+        copySample(outDir)
     }
 }
+
+//copyDistro("installDist", "main" )
+//copyDistro("installBootDist", "boot")
+
+
 
 dependencies {
     implementation("io.qpointz.mill:mill-starter-service")
